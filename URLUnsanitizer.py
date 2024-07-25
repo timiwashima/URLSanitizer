9bc8b48f50d001869271923b74e1e4344cb6c85c
timiwashima <52045865+timiwashima@users.noreply.github.com>
2024-07-25 07:02:37 -0700

    Update URLUnsanitizer.py

--- URLUnsanitizer.py
+++ URLUnsanitizer.py
@@ -7,14 +7,14 @@ Created on Tue Jul 23 08:32:44 2024
 
 import re
 
-urlList = open('c:\\dir\\to\\file.ext')
+urlList = open('c:\\users\\tiwashima\\downloads\\test.txt')
 URLs = urlList.read()
 print('Original URLs:')
 print(URLs)
 
-unsanitizedColon = re.sub('[:]', ':', URLs)
+unsanitizedColon = re.sub('\[:]', ':', URLs)
 unsanitizedHTTP = re.sub('hxxp', 'http', unsanitizedColon)
-unsanitizedURLs = re.sub('[.]', '\.', unsanitizedHTTP)
+unsanitizedURLs = re.sub('\[.]', '.', unsanitizedHTTP)
 
 
 print('\nRe-written unsanitized URLs:')
